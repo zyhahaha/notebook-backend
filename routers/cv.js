a8498002e4df597a950e863184bf3f9df8208294
zhaoyang <18355403288@163.com>
2019-05-17 14:10:35 +0800

    update cv

---
+++ routers/cv.js
@@ -0,0 +1,61 @@
+const router = require('koa-router')();
+const query = require('../lib/db');
+
+async function addCvMsg(ctx){
+  let {content, author} = ctx.request.body;
+  // let {content} = ctx.request.query;
+  let data = [content, author, Date.now()];
+  await insertData(data).then(async result => {
+    console.log(result);
+    ctx.set('Access-Control-Allow-Origin','*');
+    ctx.body = result;
+  })
+}
+async function updateNote(ctx){
+  let {id, content} = ctx.request.body;
+  let data = [content, Date.now(), id];
+  await updateData(data).then(async result => {
+    console.log(result);
+    ctx.body = result;
+  })
+}
+async function queryCvMsg(ctx){
+  await queryData().then(async result => {
+    ctx.set('Access-Control-Allow-Origin','*');
+    ctx.body = result;
+  })
+}
+async function delNote(ctx){
+  let id = 1;
+  await deleteData(data).then(async result => {
+    console.log(result);
+    ctx.body = result;
+  })
+}
+
+// add
+function insertData(data) {
+  let _sql = "insert into cv set content=?,author=?,tim=?;"
+  return query( _sql, data )
+}
+function updateData(data) {
+  let _sql = "update notes set content=?,tim=? where id=?;"
+  return query( _sql, data )
+}
+// 删除用户
+function deleteData(id) {
+  let _sql = `delete from notes where id="${id}";`
+  return query( _sql )
+}
+// 查找用户
+function queryData() {
+  let _sql = `select * from cv;`
+  return query( _sql )
+}
+
+router.post('/cv/add', addCvMsg);
+router.post('/cv/modify', updateNote);
+router.get('/cv/delete', delNote);
+router.get('/cv/query', queryCvMsg);
+
+module.exports = router
