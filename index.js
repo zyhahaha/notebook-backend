93871f138907cc98ab288420fe79b366941c8dd0
zhaoyang <18355403288@163.com>
2019-06-05 16:31:16 +0800

    update

--- index.js
+++ index.js
@@ -1,68 +1,17 @@
-// require('./lib/db');
 const Koa = require('koa');
-const path = require('path');
-// const bodyParser = require('koa-bodyparser');
 const koaBody = require('koa-body');
-const session = require('koa-session-minimal');
-// const MysqlStore = require('koa-mysql-session');
 const config = require('./config');
-const staticCache = require('koa-static-cache');
 const app = new Koa();
 
-// session存储配置
-const sessionMysqlConfig = {
-  user: config.database.USERNAME,
-  password: config.database.PASSWORD,
-  database: config.database.DATABASE,
-  host: config.database.HOST
-};
-
-// 配置session中间件
-// app.use(
-//   session({
-//     key: 'USER_SID',
-//     store: new MysqlStore(sessionMysqlConfig)
-//   })
-// );
-
-// 配置静态资源加载中间件
-// app.use(koaStatic(
-//   path.join(__dirname , './public')
-// ))
-// 缓存
 app.use(
-  staticCache(
-    path.join(__dirname, './public'),
-    { dynamic: true },
-    {
-      maxAge: 365 * 24 * 60 * 60
+  koaBody({
+    multipart: true,
+    formidable: {
+      maxFileSize: 200 * 1024 * 1024 // 设置上传文件大小最大限制，默认2M
     }
-  )
-);
-app.use(
-  staticCache(
-    path.join(__dirname, './images'),
-    { dynamic: true },
-    {
-      maxAge: 365 * 24 * 60 * 60
-    }
-  )
+  })
 );
 
-app.use(koaBody({
-  multipart: true,
-  formidable: {
-      maxFileSize: 200*1024*1024	// 设置上传文件大小最大限制，默认2M
-  }
-}));
-
-// app.use(compress({ threshold: 2048 }));
-//  路由
-// app.use(require('./routers/account/signin.js').routes());
-// app.use(require('./routers/account/signup.js').routes());
-// app.use(require('./routers/account/signout.js').routes());
-// app.use(require('./routers/notes.js').routes());
-// app.use(require('./routers/cv.js').routes());
 app.use(require('./routers/upload.js').routes());
 
 app.listen(config.port);
--- routers/account/signin.js
+++
@@ -1,7 +0,0 @@
-const router = require('koa-router')();
-const controller = require('../../controller/account/signin_ctrl')
-
-router.get('/signin', controller.getSignin)
-router.post('/signin', controller.postSignin)
-
-module.exports = router
